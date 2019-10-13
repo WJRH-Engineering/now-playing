72135865a73be3ba2d610b424e5d1e85571891da
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2019-10-13 13:57:07 -0400

    added isRobo field to tell if listening to robodj or a human

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let most_recent = {}
 
 radio.on("new-track", async function(track){
 
+	console.log(track)
+
 	//try to look up some additional metadata about this track
 	const metadata = await last_fm.lookup(track)
 
@@ -22,7 +24,7 @@ radio.on("new-track", async function(track){
 	const album = get(metadata, "track.album.title")
 	const image = get(metadata, "track.album.image.3.#text")
 
-	most_recent = { title, artist, album, image }
+	most_recent = {isRobo: track.isRobo, title, artist, album, image }
 })
 
 server.get("/now-playing", function(req, res){
--- listener.js
+++ listener.js
@@ -15,13 +15,17 @@ const tealURL = "https://api.teal.cool/organizations/wjrh/latest"
 const iceCastURL = "http://www.wjrh.org:8000/status-json.xsl"
 
 const get_latest = async function(){
+	let output = {}
 
 	// check teal to see if there is a live DJ
 	const teal_request = fetch(tealURL).then(res => res.json())
 	const teal = await teal_request
 
 	// if there is, return data about the show
-	if(teal.event != "episode-end") return teal
+	if(teal.event != "episode-end"){
+		output = {isRobo: false, ...teal}
+		return output
+	}
 
 	// if no Live DJ, ask RoboDJ what it is playing
 	const robo_request = fetch(iceCastURL).then(res => res.json())
@@ -31,11 +35,17 @@ const get_latest = async function(){
 	const data_raw = robo.icestats.source[0].title
 	const data = data_raw
 		.replace(/\[.*\]/, "")		// ignore anything between brackets
-		.split(" - ")				// data should be hypon delimited
+		.split(" - ")				// data should be hyphon delimited
 		.map(elem => elem.trim())	// trim each element
 
-	if(data.length == 2) return { title: data[0], artist: data[1] }
-	else return { title: data_raw }
+
+	if(data.length == 2){ 
+		output = {isRobo: true, title: data[0], artist: data[1] }
+	} else {
+		output = {isRobo: true, title: data_raw }
+	}
+
+	return output
 }
 
 const listen = async function(time_ms) {
@@ -45,8 +55,6 @@ const listen = async function(time_ms) {
 	while(true) {		
 		await pause(time_ms)
 		next = await get_latest()
-
-		// console.log({ next, last })
 
 		if(!equals(last, next)) radio.emit("new-track", next)
 
